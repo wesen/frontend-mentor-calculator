1551b7234280123cb807dbd240c23676d66aa60b
Manuel Odendahl <wesen@ruinwesen.com>
2022-01-02 18:15:54 -0500

    :sparkles: Add viewport setting in stories

--- src/components/TipScreen.stories.tsx
+++ src/components/TipScreen.stories.tsx
@@ -10,10 +10,22 @@ import { setBillError, setBillValue } from '../store/billSlice'
 import { setPeopleError, setPeopleValue } from '../store/peopleSlice'
 import { setCustomValue, setTipError } from '../store/tipPercentageSlice'
 
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+console.log('VIEWPORTS', INITIAL_VIEWPORTS)
+
 export default {
   component: TipScreen,
   decorators: [(story) => <Provider store={store}>{story()}</Provider>],
   title: 'TipScreen',
+  parameters: {
+    //👇 The viewports object from the Essentials addon
+    viewport: {
+      //👇 The viewports you want to use
+      viewports: INITIAL_VIEWPORTS,
+      //👇 Your own default viewport
+      defaultViewport: 'responsive',
+    },
+  },
 } as ComponentMeta<typeof TipScreen>
 
 const Template: ComponentStory<typeof TipScreen> = (args) => (
@@ -24,6 +36,14 @@ export const Default = Template.bind({})
 Default.play = () => {
   store.dispatch(reset({}))
   store.dispatch(setBillValue(3500))
+}
+
+export const DefaultMobile = Template.bind({})
+DefaultMobile.play = Default.play
+DefaultMobile.parameters = {
+  viewport: {
+    defaultViewport: 'iphonex',
+  },
 }
 
 export const BillError = Template.bind({})
